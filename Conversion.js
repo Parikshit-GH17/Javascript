58455b9589a928509b6a0cda3cc976e14f21f93d
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-12 19:54:24 +0530

    Conversion

---
+++ Conversion.js
@@ -0,0 +1,36 @@
+let score = 33
+
+
+console.log(typeof score);
+console.log(typeof(score));  // as a method
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);  
+
+//NAN STANDS FOR THE NOT A NUMBER
+
+
+//33 => 33
+// 33abd => NAN
+// 1=>true 0=> false
+   
+//BOOLEAN
+
+let isLoggedIn = true
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);  
+
+//1=> true 0=> false
+//"" => false
+//"Parikshit"=> true
+
+//STRINGS
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
