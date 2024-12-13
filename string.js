dfaf4836251a457674e8ddf47c9e7550ae860765
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-13 23:43:45 +0530

    updated

--- string.js
+++ string.js
@@ -6,23 +6,31 @@ const repoCount = 50;
 console.log(`Hello my name is ${Name} and my repocount is ${repoCount}`);
 
 const gameName = new String("ParikshitU")
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase);
-console.log(gameName.charAt(3));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
 
 
 
 const newString = "     parikshit  "
-console.log(newString);
-console.log(newString.trim());
+// console.log(newString);
+// console.log(newString.trim());
 
 const newstring = gameName.substring(0,4)
-console.log(nee=wstring);
+// console.log(nee=wstring);
 
 const anotheString = gameName.slice(-8,4)
 console.log(anotheString);
+
+
+const url = "httpss://Parikshit.com/parikshit%20u"
+console.log(url.replace('%20' , '_'));
+console.log(url.includes('Sundar'));
+console.log(gameName.split(" _ "));
+
+
 
 
 
