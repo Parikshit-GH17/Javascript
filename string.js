5dff7ac5583d52fd5463a5f540d2617be4ca3cae
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-13 23:20:34 +0530

    string method

---
+++ string.js
@@ -0,0 +1,18 @@
+const Name = "Parikshit"
+const repoCount = 50;
+
+//console.log(Name + repocount + "Value")
+
+console.log(`Hello my name is ${Name} and my repocount is ${repoCount}`);
+
+const gameName = new String("ParikshitU")
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
+
+
+
+
+
