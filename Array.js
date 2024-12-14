f7659e71a11ccecae486f63b38b34660f148fdb1
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-14 23:18:35 +0530

    Another Array Methods

--- Array.js
+++ Array.js
@@ -26,17 +26,39 @@ myArr.shift(1)
 
 // splice slice
 
-console.log("A" ,myArr)
-const myn1 = myArr.slice(1,3)
+// console.log("A" ,myArr)
+// const myn1 = myArr.slice(1,3)
 
-console.log(myn1)
-console.log("B", myArr)
+// console.log(myn1)
+// console.log("B", myArr)
 
-const myn2 = myArr.splice(1,3)
-// console.log("C" , myArr)
-console.log(myn2);
+// const myn2 = myArr.splice(1,3)
+// // console.log("C" , myArr)
+// console.log(myn2);
+
+// concat
+
+const marvel_hero = ["Thor","Iron" , "Spiderman"]
+const dc_hero = ["Superman", "Flash", "Batman"]
+
+marvel_hero.push(dc_hero)
+// console.log(marvel_hero[3],[1]);
+
+// marvel_hero.concat(dc_hero)
+// console.log(marvel_hero)
+
+// const allHeros = marvel_hero.concat(dc_hero)
+// console.log(allHeros)
 
 
+const all_new_heros = [...marvel_hero , ...dc_hero]
+// console.log(all_new_heros)
+
+ const another_Array =[1 ,2,3 ,[5,4],[ 1 , 2 , 3]]
+ const real_another_Array = another_Array.flat(Infinity)
+//  console.log(real_another_Array)
+
+console.log(Array.isArray("Parikshit"))
+console.log(Array.from("Parikshit"))
 
 
- 
