d9845eb43976d11c451ca81f5024a2426edac6fb
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-14 21:39:12 +0530

    Dates method in the Javascript

---
+++ Dates.js
@@ -0,0 +1,30 @@
+let myDate = new Date
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let myCreateDate = new Date (2024 , 0, 23);
+// let myCreateDate = new Date (2024 , 0 ,23 , 5 , 3);
+// let myCreateDate = new Date ("2024-01-14")
+let myCreateDate = new Date ("01-14-2024")
+// console.log(myCreateDate);
+
+let myTimeStamp = Date.now
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+ // TO CONVERT IT INTO THE SECONDS
+
+ //console.log(Math.floor(Date.now()/1000));
+
+ let newDate = new Date
+ console.log(newDate.getMonth());
+ console.log(newDate.getDay());
+   
+ newDate.toLocaleString('default' , {
+    weekday : "long",
+    
+})
+ 
+
