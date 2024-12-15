3ecb7895e84a8656407c64ea9f5b4a1ca8a06361
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-15 23:46:27 +0530

    updated objects

--- Objects.js
+++ Objects.js
@@ -20,15 +20,50 @@ Jsuser.Email= "dofllsl"
 
 
 Jsuser.greeting = function(){
-    console.log(" Hello JS User");
+    // console.log(" Hello JS User");
     
 }
 Jsuser.greetingTwo = function(){
-    console.log(` Hello JS User , ${this.Age}`);
+    // console.log(` Hello JS User , ${this.Age}`);
     
 }
-console.log(Jsuser.greeting());
- console.log(Jsuser.greetingTwo());
+// console.log(Jsuser.greeting());
+//  console.log(Jsuser.greetingTwo());
+
+
+// const tinderUser = new Object()//singleton
+const tinderUser = {}//non singleton object
+
+tinderUser.isLoggedIn = "false",
+tinderUser.id = "1234"
+tinderUser.name =  "Parikshit"
+
+const regularUser = {
+    email : "parik",
+    fullname  : {
+        userFullname : {
+            firstName : "Parikshit",
+            lastName : "Urkande"
+
+        }
+    }
+}
+
+console.log(regularUser.fullname.userFullname.firstName);
+//? protected by value
+
+const obj = { 1 :"a", 2 :"b"}
+const obj1 = { 3 : "c", 4 :"d"}
+ 
+//const obj2 = Object.assign({} ,obj , obj1)
+obj2 = {...obj,...obj1}
+console.log(obj2);
+
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 
